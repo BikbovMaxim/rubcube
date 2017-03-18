68e3183bed627bfef3909462fdf33eebf4bd8c0c
Bikbov Maxim <mbikbov@plgn.ru>
2017-03-18 13:48:01 +0500

    add files side.cpp and side.hpp
    this files implement class Side with all main methods

---
+++ side.cpp
@@ -0,0 +1,72 @@
+#include "side.hpp"
+
+Side::Side (std::vector< std::vector< Cell > > &cells, size_t size)
+{
+    _size = size;
+    for (size_t i = 0; i < cells.size (); ++i)
+    {
+        _side.push_back (std::vector< Cell > (cells.size ()));
+    }
+
+    for (size_t i = 0; i < cells.size (); ++i)
+    {
+        for (size_t j = 0; j < cells.size (); ++j)
+        {
+            _side[i][j] = cells[i][j];
+        }
+    }
+}
+
+Side::Side () {}
+
+Side::Side(Color c, size_t size)
+{
+    _size = size;
+    for (size_t i = 0; i < size; ++i)
+    {
+        _side.push_back (std::vector< Cell > (size));
+    }
+    for (size_t i = 0; i < size; ++i)
+    {
+        for (size_t j = 0; j < size; ++j)
+        {
+            _side[i][j] = c;
+        }
+    }
+}
+
+Side::Side (const Side &side)
+{
+    for (size_t i = 0; i < side._size; ++i)
+    {
+        _side.push_back (std::vector< Cell > (side._size));
+    }
+
+    for (size_t i = 0; i < side._size; ++i)
+    {
+        for (size_t j = 0; j < side._size; ++j)
+        {
+            _side[i][j] = side._side[i][j];
+        }
+    }
+}
+
+size_t Side::getSize () { return _size; }
+
+void Side::setCell (Cell c, size_t i, size_t j) { _side[i][j] = c; }
+
+Cell Side::getCell (size_t i, size_t j) { return _side[i][j]; }
+
+std::string Side::printSide()
+{
+    std::string output;
+    for (size_t i = 0; i < _size; ++i)
+    {
+        for(size_t j = 0; j < _size; ++j)
+        {
+            output += _side[i][j].printCell() + " ";
+        }
+        output  += "\n";
+    }
+    return output;
+}
---
+++ side.hpp
@@ -0,0 +1,26 @@
+#ifndef __SIDE_HPP__
+#define __SIDE_HPP__
+
+#include <iostream>
+#include <vector>
+#include "types.hpp"
+#include "cell.hpp"
+
+class Side
+{
+public:
+    Side ();
+    Side (Color c, size_t size);
+    Side (std::vector< std::vector< Cell > > &cells, size_t size);
+    Side (const Side &side);
+    size_t getSize ();
+    void setCell (Cell c, size_t i, size_t j);
+    Cell getCell (size_t i, size_t j);
+    std::string printSide ();
+
+private:
+    size_t _size;
+    std::vector< std::vector< Cell > > _side;
+};
+
+#endif /* __SIDE_HPP__ */
