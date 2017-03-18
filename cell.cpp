0ca54dde327a4950f81c5a8288e5b72ac58dd945
Bikbov Maxim <mbikbov@plgn.ru>
2017-03-18 13:46:41 +0500

    add files cell.cpp and cell.hpp
    this files implement class Cell with all main methods

---
+++ cell.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include "cell.hpp"
+
+Cell::Cell () {}
+
+Cell::Cell (Color c) { _color = c; }
+
+Cell::Cell (const Cell &c) { _color = c._color; }
+
+Color Cell::getColor () { return _color; }
+
+void Cell::setColor (Color c) { _color = c; }
+
+std::string Cell::printCell()
+{
+    switch (_color)
+    {
+    case Color::Blue:
+        return std::string("B");
+    case Color::Green:
+        return std::string("G");
+    case Color::Orange:
+        return std::string("O");
+    case Color::Red:
+        return  std::string("R");
+    case Color::White:
+        return std::string("W");
+    case Color::Yellow:
+        return std::string("Y");
+    }
+}
---
+++ cell.hpp
@@ -0,0 +1,20 @@
+#ifndef __CELL_HPP__
+#define __CELL_HPP__
+
+#include "types.hpp"
+
+class Cell
+{
+public:
+    Cell ();
+    Cell (Color c);
+    Cell (const Cell &c);
+    Color getColor ();
+    void setColor (Color c);
+    std::string printCell ();
+
+private:
+    Color _color;
+};
+
+#endif /* __CELL_HPP__ */
